46fd11bdb56a03615ef0f3d3d17283554c7d9767
slowbs <poison_word@hotmail.com>
2019-10-28 14:19:53 +0700

    sidebar fin before add
    auth-sidebar edit page fin before add page

--- memberWorkshop/src/app/authentication/authentication.module.ts
+++ memberWorkshop/src/app/authentication/authentication.module.ts
@@ -5,11 +5,14 @@ import { AuthenticationRoutingModule } from './authentication.routing';
 import { SharedsModule } from '../shareds/shareds.module';
 import { SettingComponent } from './components/setting/setting.component';
 import { ProfileComponent } from './components/profile/profile.component';
+import { BootstrapElementsComponent } from './components/bootstrap-elements/bootstrap-elements.component';
+import { CardsComponent } from './components/cards/cards.component';
+import { WidgetsComponent } from './components/widgets/widgets.component';
 
 
 
 @NgModule({
-  declarations: [DashboardComponent, SettingComponent, ProfileComponent],
+  declarations: [DashboardComponent, SettingComponent, ProfileComponent, BootstrapElementsComponent, CardsComponent, WidgetsComponent],
   imports: [
     CommonModule,
     AuthenticationRoutingModule,
--- memberWorkshop/src/app/authentication/authentication.routing.ts
+++ memberWorkshop/src/app/authentication/authentication.routing.ts
@@ -4,6 +4,9 @@ import { AuthURL } from './authentication.url';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { SettingComponent } from './components/setting/setting.component';
 import { ProfileComponent } from './components/profile/profile.component';
+import { BootstrapElementsComponent } from './components/bootstrap-elements/bootstrap-elements.component';
+import { CardsComponent } from './components/cards/cards.component';
+import { WidgetsComponent } from './components/widgets/widgets.component';
 
 
 
@@ -12,7 +15,10 @@ const routes: Routes = [
     { path: '', redirectTo: AuthURL.Dashboard, pathMatch: 'fulll'},
     { path: AuthURL.Dashboard, component: DashboardComponent },
     { path: AuthURL.Setting, component: SettingComponent},
-    { path: AuthURL.Profile, component: ProfileComponent}
+    { path: AuthURL.Profile, component: ProfileComponent},
+    { path: AuthURL.Element, component: BootstrapElementsComponent},
+    { path: AuthURL.Card, component: CardsComponent},
+    { path: AuthURL.Widget, component: WidgetsComponent}
 
 ];
 
