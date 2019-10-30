90d51de679487463155285bd44de67b9cf9a69b4
slowbs <poison_word@hotmail.com>
2019-10-30 16:20:17 +0700

    login interface

--- memberWorkshop/src/app/components/login/login.component.ts
+++ memberWorkshop/src/app/components/login/login.component.ts
@@ -1,17 +1,45 @@
 import { Component, OnInit } from '@angular/core';
 import { AppURL } from '../../app.url';
+import { ILoginComponent } from './login.interface';
+import { FormBuilder, Validators } from '@angular/forms';
+import { alertService } from 'src/app/shareds/services/alert.service';
+import { Router } from '@angular/router';
+import { AuthURL } from 'src/app/authentication/authentication.url';
 
 @Component({
-  selector: 'app-login',
-  templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+    selector: 'app-login',
+    templateUrl: './login.component.html',
+    styleUrls: ['./login.component.css']
 })
-export class LoginComponent implements OnInit {
+export class LoginComponent implements ILoginComponent {
+    constructor(
+        private builder: FormBuilder,
+        private alert: alertService,
+        private router: Router
+    ) {
+        this.initialCreateFormData();
+    }
 
-  constructor() { }
+    Url = AppURL;
+    form: import("@angular/forms").FormGroup;
 
-  ngOnInit() {
-  }
+    //เข้าสู่ระบบ
+    onSubmit() {
+        //throw new Error("Method not implemented.");
+        if (this.form.invalid)
+            return this.alert.something_wrong();
+        this.router.navigate(['/', AppURL.Authen, AuthURL.Dashboard])
+        //console.log(this.form.value)
+    }
+    void: any;
 
-  Url = AppURL;
+    //สร้างฟอร์ม
+    private initialCreateFormData() {
+        this.form = this.builder.group({
+            username: ['', Validators.required],
+            password: ['', Validators.required],
+            remember: [true]
+        })
+    }
+
 }
